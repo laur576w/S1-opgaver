9db4ebf022a8a974026ce1edf30fc368d8f34990
laur576w <114057522+laur576w@users.noreply.github.com>
2022-09-26 14:40:30 +0200

    til opgave 15

--- Opgave14/Program.cs
+++ Opgave14/Program.cs
@@ -1,6 +1,57 @@
-﻿//opgave 14.1
+﻿int i;
+//opgave 14.1 & 14.2
 List<int> ages = new List<int>() { 5, 17, 20, 40, 51};
 
 List<string> names = new List<string>() { "Jens", "Karl", "Johannes", "Anton", "Nicklas" };
 
+List<double> percentages = new List<double>() { 0.75, 0.23, 0.86, 0.17 };
 
+List<bool> are_married = new List<bool>() { true, false, false, true, true };
+
+
+//opgave 14.3
+for (i = 0; i < ages.Count; i++)
+    Console.WriteLine(ages[i]);
+for (i = 0; i < names.Count; i++)
+    Console.WriteLine(names[i]);
+
+foreach (double percentage in percentages)
+    Console.WriteLine(percentage);
+foreach (bool is_married in are_married)
+    Console.WriteLine(is_married);
+
+Console.WriteLine();
+
+
+//opgave 14.4
+List<string> names1 = new List<string>() { "Hans", "Kristian", "Jens", "Karsten", "Ib" };
+
+foreach (string name in names1)
+    Console.WriteLine(name);
+
+names1.Insert(3, "Anders");
+names1.Insert(2, "Lars");
+
+foreach (string name in names1)
+    Console.WriteLine(name);
+
+Console.WriteLine();
+
+
+//opgave 14.5
+List<int> ages1 = new List<int>() { 13, 14, 13, 15, 13, 14, 14, 15 };
+foreach (int age in ages1)
+    Console.WriteLine(age);
+
+Console.WriteLine(); //air
+
+ages1.Remove(13);
+ages1.Remove(15); //current line 14, 13, 13, 14, 14, 15
+foreach (int age in ages1)
+    Console.WriteLine(age);
+
+Console.WriteLine(); //air
+
+ages1.RemoveAt(3); //current line 14, 13, 13, 14 ,15
+foreach (int age in ages1)
+    Console.WriteLine(age);
---
+++ Opgave15/Program.cs
@@ -0,0 +1,51 @@
+﻿DateTime now = DateTime.Now; 
+int year = now.Year;
+string input = "";
+string countDown;
+
+while (input.ToLower() != "exit") {
+    Console.WriteLine("Skriv (0) for at få idags dato og tid");
+    Console.WriteLine("Skriv (1) for at finde ud af hvor mange dage der er til jul");
+    Console.WriteLine("Skriv (2) for at finde ud af hvor lnag tid til min fødselsdag");
+    Console.WriteLine("Skriv (2) for at finde ud af hvor lnag tid til din dato");
+
+    //check which they wanna check or if exit
+    input = Console.ReadLine();
+
+    switch (input) {
+        case "0":
+            //today
+            Console.WriteLine(now.ToString());
+            break;
+
+        case "1":
+            //xmas time
+            DateTime xmas = DateTime.Parse($"24/12/{year}");
+
+             //Calculate countdown timer.
+             TimeSpan till_xmas = xmas - now;
+             countDown = string.Format("{0} Days, {1} Hours, {2} Minutes, {3} Seconds to christmas.", till_xmas.Days, till_xmas.Hours, till_xmas.Minutes, till_xmas.Seconds);
+             Console.WriteLine(countDown);
+             break;
+            
+        case "2":
+              //birthday time
+             DateTime bday = DateTime.Parse($"22/11/{year}");
+
+             TimeSpan till_bday = bday - now;
+             countDown = string.Format("{0} Days, {1} Hours, {2} Minutes, {3} Seconds to birthday.", till_bday.Days, till_bday.Hours, till_bday.Minutes, till_bday.Seconds);
+             Console.WriteLine(countDown);
+             break;
+
+        case "3":
+            //their time
+            Console.WriteLine("Skriv med numre dag/måned/årstal");
+            DateTime time_input = DateTime.Parse(Console.ReadLine());
+            TimeSpan till_x = time_input - now;
+            countDown = string.Format("{0} Days, {1} Hours, {2} Minutes, {3} Seconds to your time.", till_x.Days, till_x.Hours, till_x.Minutes, till_x.Seconds);
+            Console.WriteLine(countDown);
+            break;
+    }
+    Console.WriteLine("\n" +
+        "Skriv exit for at forlade progammet");
+}
