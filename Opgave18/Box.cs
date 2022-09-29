1959050fa1666c6ca75b004932e99ef6435359b5
laur576w <114057522+laur576w@users.noreply.github.com>
2022-09-29 10:23:57 +0200

    Opgave 18 WIP

--- Opgave18/Box.cs
+++ Opgave18/Box.cs
@@ -1,19 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Opgave18 {
     internal class Box  {
-        public int Length { get; }
-        public int Width { get; }
-        public int Height { get; }
+        public double Length { get; }
+        public double Width { get; }
+        public double Height { get; }
         
-        public Box(int width, int height, int length) {
+        public Box(double width, double height, double length) {
             Console.WriteLine("width = " + width);
             Console.WriteLine("height = " + height);
             Console.WriteLine("length = " + length);
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public void PrintInfo() {
+
+            Console.WriteLine("hej");
         }
     }
+    
 }
--- Opgave18/Program.cs
+++ Opgave18/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using Opgave18;
+
+Box box1 = new Box(70, 65, 80);
+
+Console.WriteLine(box1.PrintInfo());
