c5efa313d00b4b3ec996264b6bedfcb313eed20d
laur576w <114057522+laur576w@users.noreply.github.com>
2022-09-29 10:00:54 +0200

    Til opgave 18

--- Opgave18/Box.cs
+++ Opgave18/Box.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace Opgave18 {
     internal class Box  {
-        public Box(double width, double height, double length) {
+        public int Length { get; }
+        public int Width { get; }
+        public int Height { get; }
+        
+        public Box(int width, int height, int length) {
             Console.WriteLine("width = " + width);
             Console.WriteLine("height = " + height);
             Console.WriteLine("length = " + length);
         }
-
     }
 }
