8fd5b57f7596720ef17fcae5c3b532e0fc196607
laur576w <114057522+laur576w@users.noreply.github.com>
2022-10-05 14:43:49 +0200

    Opgave 20

--- Opgave20/Program.cs
+++ Opgave20/Program.cs
@@ -1,1 +1,17 @@
 ﻿using System.IO;
+
+using (StreamReader sr = new("Values.txt")) {
+    string line;
+    int sum = 0;
+    double i = 0;
+    while ((line = sr.ReadLine()) != null) {
+        int number = int.Parse(line);
+        Console.WriteLine(number);
+        sum = sum + number;
+        i++;
+    }
+    Console.WriteLine("det her er sum {0}", sum);
+    i = sum / i;
+    Console.WriteLine("gennemsnittet er {0}", i);
+}
+
